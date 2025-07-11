740b11bc32c83170e487664a288f191d11725245
hanto0916 <hanto0916@gmail.com>
2025-07-11 11:40:22 +0900

    연습문제

---
+++ JavaTest/src/test1/Test03.java
@@ -0,0 +1,28 @@
+package test1;
+
+/*
+ * 날짜 : 2025/07/09
+ * 이름 : 한탁원
+ * 내용 : 자바 연산자 연습문제
+ */
+public class Test03 {
+
+	public static void main(String[] args) {
+
+		int result;
+		int num = 1;
+		
+		result = num++;
+		System.out.println("result : " + result);
+		
+		result = ++num;
+		System.out.println("result : " + result);
+		
+		result = num--;
+		System.out.println("result : " + result);
+		
+		result = --num;
+		System.out.println("result : " + result);
+		
+	}
+}
---
+++ JavaTest/src/test1/Test08.java
@@ -0,0 +1,26 @@
+package test1;
+
+/*
+ * 날짜 : 2025/07/09
+ * 이름 : 한탁원
+ * 내용 : 자바 반복문 연습문제
+ */
+public class Test08 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		
+		for(int i=0; i<5; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i + 1; j++) {
+                System.out.print("*");
+            }
+			
+			System.out.print("\n");
+		}
+	}
+
+}
