1ef6eebdf31543f8f496d42d88f776a8ad8d6686
hanto0916 <hanto0916@gmail.com>
2025-07-16 16:12:45 +0900

    first commit

--- ch05/src/sub6/PolyTest.java
+++ ch05/src/sub6/PolyTest.java
@@ -46,6 +46,32 @@ public class PolyTest {
 		Animal arr[] = {t, e, s};
 		
 		arr[0].move();
+		
+		/*
+		다형성 확인문제   
+		
+		1) Pet 클래스를 정의하시오.   	
+		 - 클래스 내용은 아래 내용을 참고해 작성하세요.   
+		
+		2) Pet 클래스를 상속받는 Dog, Cat 클래스를 정의하시오.   
+		 - Dog, Cat 클래스는 makeSound() 메서드를 재정의 하세요.   
+		 - Dog는 "멍멍" 출력   
+		 - Cat 은 "야옹" 출력   
+		
+		3) Main 클래스에서 Dog, Cat 객체를 생성하고 정적 메서드 printSound() 를 정의하시오.   
+		 - main() 메서드에서 printSound() 메서드를 호출하세요.   
+		 - printSound()는 Dog, Cat 객체를 인자로 받는 매개변수를 선언하세요.
+		 - printSound()는 매개변수를 이용해 Dog, Cat의 makeSound()를 호출하세요.
+	*/
+		
+		Pet dog = new Dog();
+		Pet cat = new Cat();
+		
+		printSound(dog);
+		printSound(cat);
 	}
 	
+	public static void printSound(Pet pet) {
+		pet.makeSound();
+	}
 }
